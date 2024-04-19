3cb6f6d30605f710a4dfc6ccbafce9b442ba6b4f
JhordanKS <jhordan.kendy1@gmail.com>
2024-04-19 15:06:31 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Ola eu sou o Goku</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <main>
+      <section></section>
+
+      <section></section>
+
+      <section></section>
+    </main>
+  );
+};
+
+export default Home;
